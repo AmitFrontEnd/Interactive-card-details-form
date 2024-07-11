063a2cd67916ee6f0deb7100dea1e3a5ac51ce11
AmitKumar1590 <164554764+AmitKumar1590@users.noreply.github.com>
2024-07-11 15:04:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,37 +54,46 @@ numberInput.addEventListener('input', () => {
 });
 
 month.addEventListener('input', () => {
-    if (month.value.match(/^[0-9]{1,2}$/) && month.value <= 12) {
-        month.classList.remove('border-red');
-        month.nextElementSibling.style.display = 'none';
-    } else {
+    if (!month.value.match(/^[0-9]*$/)) {
         month.value = month.value.replace(/[^0-9]/g, '');
         month.nextElementSibling.style.display = 'block';
         month.classList.add('border-red');
+    } else if (month.value > 12 || month.value.length > 2) {
+        month.nextElementSibling.style.display = 'block';
+        month.classList.add('border-red');
+    } else {
+        month.classList.remove('border-red');
+        month.nextElementSibling.style.display = 'none';
     }
     validateForm();
 });
 
 year.addEventListener('input', () => {
-    if (year.value.match(/^[0-9]{2}$/)) {
-        year.classList.remove('border-red');
-        year.nextElementSibling.style.display = 'none';
-    } else {
+    if (!year.value.match(/^[0-9]*$/)) {
         year.value = year.value.replace(/[^0-9]/g, '');
         year.nextElementSibling.style.display = 'block';
         year.classList.add('border-red');
+    } else if (year.value.length > 2) {
+        year.nextElementSibling.style.display = 'block';
+        year.classList.add('border-red');
+    } else {
+        year.classList.remove('border-red');
+        year.nextElementSibling.style.display = 'none';
     }
     validateForm();
 });
 
 cvc.addEventListener('input', () => {
-    if (cvc.value.match(/^[0-9]{3}$/)) {
-        cvc.classList.remove('border-red');
-        cvc.nextElementSibling.style.display = 'none';
-    } else {
+    if (!cvc.value.match(/^[0-9]*$/)) {
         cvc.value = cvc.value.replace(/[^0-9]/g, '');
         cvc.nextElementSibling.style.display = 'block';
         cvc.classList.add('border-red');
+    } else if (cvc.value.length > 3) {
+        cvc.nextElementSibling.style.display = 'block';
+        cvc.classList.add('border-red');
+    } else {
+        cvc.classList.remove('border-red');
+        cvc.nextElementSibling.style.display = 'none';
     }
     validateForm();
 });
