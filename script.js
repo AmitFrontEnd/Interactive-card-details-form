e646f9c34e525bf4a76dd63a02373eaa44ab734b
AmitKumar1590 <164554764+AmitKumar1590@users.noreply.github.com>
2024-07-11 14:51:50 +0530

    Updated validations in javascript file

--- script.js
+++ script.js
@@ -6,28 +6,28 @@ let year = document.querySelector('.year');
 let cvc = document.querySelector('.cvc');
 let cardNumber = document.querySelector('.c-number');
 let ownerName = document.querySelector('.name');
-let error1=document.querySelector('.er1')
-let error2=document.querySelector('.er2')
-let date=document.querySelector('.date')
-let ryear=document.querySelector('.ryear')
-let cvv=document.querySelector('.cvv')
-let nameText=document.querySelector('.name')
-let form=document.querySelector('form')
-let thankYou=document.querySelector('.second')
-let subBtn=document.querySelector('.submit')
+let error1 = document.querySelector('.er1');
+let error2 = document.querySelector('.er2');
+let date = document.querySelector('.date');
+let ryear = document.querySelector('.ryear');
+let cvv = document.querySelector('.cvv');
+let nameText = document.querySelector('.name');
+let form = document.querySelector('form');
+let thankYou = document.querySelector('.second');
+let subBtn = document.querySelector('.submit');
 
 const validateForm = () => {
     // Check number input
     const isNumberValid = numberInput.value.match(/^[0-9 ]*$/) && numberInput.value.replace(/\s+/g, '').length === 16;
     
     // Check month input
-    const isMonthValid = month.value !== '';
+    const isMonthValid = month.value.match(/^[0-9]{1,2}$/) && month.value <= 12;
     
     // Check year input
-    const isYearValid = year.value !== '';
+    const isYearValid = year.value.match(/^[0-9]{2}$/);
     
     // Check CVC input
-    const isCVCValid = cvc.value !== '';
+    const isCVCValid = cvc.value.match(/^[0-9]{3}$/);
     
     // Check name input
     const isNameValid = nameInput.value !== '';
@@ -43,65 +43,56 @@ numberInput.addEventListener('keyup', () => {
         // Format the input value with spaces after every 4 digits
         numberInput.value = cleanedValue.replace(/(\d{4})(?=\d)/g, "$1 ");
         cardNumber.value = numberInput.value;
-        numberInput.classList.remove('border-red')
+        numberInput.classList.remove('border-red');
 
         numberInput.nextElementSibling.style.display = 'none';
     } else {
         numberInput.nextElementSibling.style.display = 'block';
-        numberInput.classList.add('border-red')
+        numberInput.classList.add('border-red');
     }
     validateForm();
 });
 
 month.addEventListener('keyup', () => {
-    if (month.value === '') {
-        error2.style.display = 'block';
-        month.classList.add('border-red');
-    } else {
-        error2.style.display = 'none';
+    if (month.value.match(/^[0-9]{1,2}$/) && month.value <= 12) {
         month.classList.remove('border-red');
-        date.innerText = month.value;
+        month.nextElementSibling.style.display = 'none';
+    } else {
+        month.nextElementSibling.style.display = 'block';
+        month.classList.add('border-red');
     }
     validateForm();
 });
 
 year.addEventListener('keyup', () => {
-    if (year.value === '') {
-        error2.style.display = 'block';
-        year.classList.add('border-red');
-    } else {
-        error2.style.display = 'none';
+    if (year.value.match(/^[0-9]{2}$/)) {
         year.classList.remove('border-red');
-        ryear.innerText = year.value;
+        year.nextElementSibling.style.display = 'none';
+    } else {
+        year.nextElementSibling.style.display = 'block';
+        year.classList.add('border-red');
     }
     validateForm();
 });
 
 cvc.addEventListener('keyup', () => {
-    if (cvc.value === '') {
-        error1.style.display = 'block';
-        cvc.classList.add('border-red');
-    } else {
-        error1.style.display = 'none';
+    if (cvc.value.match(/^[0-9]{3}$/)) {
         cvc.classList.remove('border-red');
-        cvv.innerText = cvc.value;
+        cvc.nextElementSibling.style.display = 'none';
+    } else {
+        cvc.nextElementSibling.style.display = 'block';
+        cvc.classList.add('border-red');
     }
     validateForm();
 });
 
-nameInput.addEventListener('input', () => {
-    nameText.innerText = nameInput.value;
+nameInput.addEventListener('keyup', () => {
+    if (nameInput.value !== '') {
+        nameInput.classList.remove('border-red');
+        nameInput.nextElementSibling.style.display = 'none';
+    } else {
+        nameInput.nextElementSibling.style.display = 'block';
+        nameInput.classList.add('border-red');
+    }
     validateForm();
 });
-
-subBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (!subBtn.disabled) {
-        form.style.display = 'none';
-        thankYou.style.display = 'block';
-    }
-});
-
-// Initial validation check
-validateForm();
-
